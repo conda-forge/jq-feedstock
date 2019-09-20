bc9437f932ce99d3ff2f699266b43794364191d3
Uwe L. Korn <uwe.korn@quantco.com>
2019-09-20 15:14:05 +0200

    Add windows build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -x
+
 chmod +x configure
 
 ./configure --disable-maintainer-mode --prefix=$PREFIX --with-oniguruma=$PREFIX
+[[ "$target_platform" == "win-64" ]] && patch_libtool && sed -e 's/"/\\"/g' -e 's/^/"/' -e 's/$$/\\n"/' src/builtin.jq > src/builtin.inc
 
 make -j${CPU_COUNT}
 make check
