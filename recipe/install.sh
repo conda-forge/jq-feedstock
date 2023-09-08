1037bf6b680d2ddc487149f1489b72ef4d93d82b
Ken Odegard <kodegard@anaconda.com>
2023-09-08 11:40:47 -0500

    Convert paths

--- recipe/install.sh
+++ recipe/install.sh
@@ -2,9 +2,18 @@
 set -ex
 
 if [[ $(uname) =~ MSYS.* ]]; then
+    PREFIX="$(cygpath "${PREFIX}")"
     CFLAGS="-I${PREFIX}/Library/mingw-w64/x86_64-w64-mingw32/include"
     ONIGURUMA="${PREFIX}/Library"
 fi
+
+echo ./configure \
+    --disable-docs \
+    --disable-valgrind \
+    --disable-maintainer-mode \
+    --prefix="${PREFIX}" \
+    --with-oniguruma="${ONIGURUMA}" \
+    CFLAGS="${CFLAGS}"
 
 ./configure \
     --disable-docs \
