addca903b5e8fbb628efa050157ed1d43f4c1c4b
Ken Odegard <kodegard@anaconda.com>
2023-09-07 22:10:59 -0500

    Alternative tarball

--- recipe/install.sh
+++ recipe/install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -ex
 
-autoreconf \
-    --install \
-    --force \
-    --verbose
+if [[ $(uname) =~ MSYS.* ]]; then
+    CFLAGS="-I${LIBRARY_PREFIX}/mingw-w64/x86_64-w64-mingw32/include/"
+fi
 
 ./configure \
+    --disable-docs \
+    --disable-valgrind \
     --disable-maintainer-mode \
     --prefix="${PREFIX}" \
-    --with-oniguruma="${PREFIX}"
+    --with-oniguruma=${LIBRARY_PREFIX:-${PREFIX}} \
+    CFLAGS="${CFLAGS}"
+
 
 make -j${CPU_COUNT}
-[[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]] && make check
+[[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]] && make check VERBOSE=yes
 make install
