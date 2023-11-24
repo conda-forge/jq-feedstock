0847fa9ab4f590436f7513e6187f774a8b1fddbb
Bela Stoyan <bela.stoyan@gmail.com>
2023-11-24 11:27:03 +0100

    only update gnuconfig on unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
+if [[ "$target_platform" != "win-64" ]]; then
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./modules/oniguruma
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
+fi
 
 chmod +x configure
 
