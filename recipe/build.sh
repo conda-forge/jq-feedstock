23f9c89be316043d11c9662104ef5c136b862a64
Yuanyuan Wei <aywi@users.noreply.github.com>
2025-07-05 20:26:12 +0000

    Add Windows build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,10 @@ set -ex
 chmod +x configure
 
 ./configure --prefix=$PREFIX --with-oniguruma=$PREFIX
+[[ "$target_platform" == "win-64" ]] && patch_libtool
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+    make check
 fi
-
 make install
