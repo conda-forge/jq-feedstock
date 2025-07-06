bf6d5554bf706e5eb901123ef78629e00b0994ac
Yuanyuan Wei <aywi@users.noreply.github.com>
2025-07-06 00:29:58 +0000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -ex
 
 chmod +x configure
 
+[[ "$target_platform" == "win-64" ]] && export CFLAGS="$CFLAGS -Disatty=_isatty -DWIN32 -DYY_NO_UNISTD_H -include io.h" && cd $(pwd)
 ./configure --prefix=$PREFIX --with-oniguruma=$PREFIX
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 
