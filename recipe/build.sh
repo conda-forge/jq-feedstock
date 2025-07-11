3ce3b7e79ac0490608210ac5a2c91173dd82ab85
Yuanyuan Wei <aywi@users.noreply.github.com>
2025-07-11 05:17:06 +0000

    Try m2w64_c + m2-*

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,21 +4,7 @@ set -ex
 
 chmod +x configure
 
-if [[ "$target_platform" == "win-64" ]]; then
-#     # Fix `src/lexer.c:839:10: fatal error: 'unistd.h' file not found`, `src/lexer.h:474:10: fatal error: 'unistd.h' file not found`
-#     export CFLAGS="$CFLAGS -DYY_NO_UNISTD_H"
-#     # Fix `src/lexer.c:2041:40: error: call to undeclared function 'isatty'; ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]`
-#     # https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/posix-isatty?view=msvc-170
-#     # https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/isatty?view=msvc-170
-#     export CFLAGS="$CFLAGS -include io.h"
-#     # `src/main.c:4:10: fatal error: 'libgen.h' file not found`
-#     # `src/builtin.c:11:10: fatal error: 'sys/time.h' file not found`
-#     export CFLAGS="$CFLAGS -DWIN32"
-    # Fix `configure: error: unsafe absolute working directory name`
-    cd $(pwd)
-fi
 ./configure --prefix=$PREFIX --with-oniguruma=$PREFIX
-[[ "$target_platform" == "win-64" ]] && patch_libtool
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
